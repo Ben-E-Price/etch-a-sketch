88be81f6c7fc51db817959c768624bde7d295b57
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-04-12 18:36:42 +0100

    button-panel.js created Button ButtonPanel added to button-panel.js

---
+++ project/src/button-panel.js
@@ -0,0 +1,21 @@
+// Retruns indivdual button element
+const Button = (prop) => {
+    return(
+        <button>
+            {prop.textCont}
+        </button>
+    )
+};
+
+const ButtonPanel = () => {
+    const controls = ["colour mode", "colour fill", "easer", "clear board", "toggle lines"];
+    const buttons = controls.map((string) => <Button textCont={string}/>);
+
+    return(
+        <div id="button-wrapper">
+            {buttons}
+        </div>
+    )
+};
+
+export default ButtonPanel
--- project/src/controls.js
+++ project/src/controls.js
@@ -1,22 +1,4 @@
-// Retruns indivdual button element
-const Button = (prop) => {
-    return(
-        <button>
-            {prop.textCont}
-        </button>
-    )
-};
-
-const ButtonPanel = () => {
-    const controls = ["colour mode", "colour fill", "easer", "clear board", "toggle lines"];
-    const buttons = controls.map((string) => <Button textCont={string}/>);
-
-    return(
-        <div id="button-wrapper">
-            {buttons}
-        </div>
-    )
-};
+import ButtonPanel from "./button-panel";
 
 const ControlsPanel = () => {
     
