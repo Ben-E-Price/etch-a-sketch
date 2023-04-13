8bc196fa7739ebe46b88de4d4e7b95c77c5442e0
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-04-13 20:09:46 +0100

    ResolutionInput added current resolution display

--- project/src/resolution-input.js
+++ project/src/resolution-input.js
@@ -1,7 +1,23 @@
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+
 const ResolutionInput = () => {
+    const ref = useRef("");
+    const [currentRes, updateCurrentRes] = useState("");
+
+    //Update currentRes value
+    const setRes = (ref) => {
+        updateCurrentRes(ref.value)
+    };
+    
+    // Initilize on first render
+    useEffect(() => {
+        setRes(ref.current);
+    }, []);
+
     return(
         <>
-            <input type="range" min={0} max={100}></input>
+            <p>{currentRes}</p>
+            <input type="range" min={4} max={32} step={4} ref={ref} onChange={(el) => setRes(el.target)}></input>
         </>
     )
 };
