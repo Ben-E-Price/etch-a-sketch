31d1824532171b987caa8255c9efd6f97a2b8f26
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-04-13 19:21:36 +0100

    File resolution-input Component ResoultionInput created

--- project/src/controls.js
+++ project/src/controls.js
@@ -1,9 +1,11 @@
 import ButtonPanel from "./button-panel";
+import ResolutionInput from "./resolution-input";
 
 const ControlsPanel = () => {
     
     return(
         <div id="control-wrapper">
+            <ResolutionInput />
             <ButtonPanel />
         </div>
     )
---
+++ project/src/resolution-input.js
@@ -0,0 +1,9 @@
+const ResolutionInput = () => {
+    return(
+        <>
+            <input type="range" min={0} max={100}></input>
+        </>
+    )
+};
+
+export default ResolutionInput
