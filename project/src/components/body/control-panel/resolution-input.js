0f48957b91b664da773ddca8567f2f25a7c87514
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-04-25 16:36:09 +0100

    setRes function moved to ResoultionInput Passing currentRes to GridResoultion

--- project/src/components/body/control-panel/resolution-input.js
+++ project/src/components/body/control-panel/resolution-input.js
@@ -1,10 +1,42 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useGridResolution from "../../../hooks/grid-res";
 
 const ResolutionInput = () => {
     const ref = useRef("");
-    const {currentRes, inputStep, inputMin} = useGridResolution(ref);
+    const [currentRes, updateCurrentRes] = useState(16);
+    const [inputStep, updateInputStep] = useState(currentRes);
+    const [inputMin, updateInputMin] = useState(0);
+    useGridResolution(currentRes)
+
+    // Increase/Decrease resolution value, maintaing powers of 2
+    const setRes = (ref) => {
+        const prevRes = currentRes;
+        const currentValue = ref.value;
+
+        // Blocks below minimum input values - Sets input elements min value 
+        const inputLimit = (inputValue) => {
+            const minValue = 4;
+            inputValue <= minValue ? updateInputMin(minValue) : updateInputMin(0);
+        };
         
+        // Update inputStep value - Increase resoulution value
+        const resIncrease = () => {
+            updateInputStep(currentRes);
+            updateCurrentRes(currentRes * 2);
+        };
+        
+        // Update inputStep value - Decrease resoulution value 
+        const resDecrease = () => {
+            const decreasedRes = currentRes / 2;
+
+            updateInputStep(decreasedRes);
+            updateCurrentRes(decreasedRes);
+        };
+
+        inputLimit(currentValue);
+        prevRes < currentValue ? resIncrease() : resDecrease();
+    };
+    
     return(
         <>
             <p>Resoulution {currentRes} X {currentRes}</p>
@@ -15,8 +47,9 @@ const ResolutionInput = () => {
                 max={64} 
                 value={currentRes} 
                 step={inputStep} 
-                ref={ref} 
-                onInput={(el) => setRes(el.target)}>
+                ref={ref}
+                onInput={(el) => {setRes(el.target)}}
+                >
             </input>
         </>
     )
