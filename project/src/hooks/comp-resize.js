3b4dd72ec743335a74e35ca7fb0c2c85a7cd0113
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-04-28 19:49:34 +0100

    comp-resize hook resizeComp removed Calls replaced with getCompSize

--- project/src/hooks/comp-resize.js
+++ project/src/hooks/comp-resize.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const useCompResize = (ref) => {
     const [width, setCompWidth] = useState(0);
@@ -8,25 +8,14 @@ const useCompResize = (ref) => {
         setCompWidth(ref.current.getBoundingClientRect().width);
     };
 
-    //Update componets sizing - Ensures component width:height - 1:1
-    const resizeComp = useCallback((ref, size) => {
-        const applySize = (ref, size) => {
-            ref.current.style.height = `${size}px`;
-            // console.log(ref.current.getBoundingClientRect())
-        }; 
-        
-        getCompSize(ref);
-        // applySize(ref, width);
-    }, [width]);
-
     useEffect(() => {
-        resizeComp(ref, width)
+        getCompSize(ref)
         window.addEventListener("resize",() => getCompSize(ref));
 
         return () => {
             window.removeEventListener("resize",() => getCompSize(ref));
         };
-    }, [ref, width, resizeComp]);
+    }, [ref]);
 
     return {width};
 };
