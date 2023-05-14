36f26c9634eead13af6c3641ca299a4559bcbb79
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-05-14 19:54:09 +0100

    pixel.js Created function updatePixelColour Sets compoent background colour

--- project/src/components/body/board/pixel.js
+++ project/src/components/body/board/pixel.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import useColourMode from "../../../hooks/colour-mode";
+
 function Pixel(prop) {
-    // const sizeString = prop.sizing
+    const [pixelColour, setPixelColour] = useState("");
+    const {activeMode} = useColourMode();
     
+    // Set background colour of component
+    const updatePixelColour = (ref) => {
+        setPixelColour(activeMode.colour);
+        ref.style.background = pixelColour;
+    };
+
     return(
-        <div className='pixel' style={{width: prop.sizing, height: prop.sizing}}>
+        <div 
+            className='pixel'
+            style={{width: prop.sizing, height: prop.sizing}}
+            onClick={(element) => {updatePixelColour(element.target)}}>
         </div>
     )
 };
