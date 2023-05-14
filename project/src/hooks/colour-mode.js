6c3cc686ee155ff31552e49a084d11fbf4f648a7
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-05-14 09:48:38 +0100

    Declared mode map Contains 3 empty objects

--- project/src/hooks/colour-mode.js
+++ project/src/hooks/colour-mode.js
@@ -1,8 +1,22 @@
 import { useState } from "react";
 
+// Contains mode objects
+const modes = new Map(
+    [0, {}],
+    [1, {}],
+    [2, {}],
+);
+
+//Cycle + retrun active mode object
 const useColourMode = (force, forceMode) => {
     const [activeMode, setActiveMode] = useState("");
     const [cycleMode, setCycleMode] = useState("");
+
+    //Increment cycleMode value, Resets value = 1 if modeLimit met
+    const switchMode = () => {
+        const modeLimit = 2;
+        cycleMode >= modeLimit ? setCycleMode(1) : setCycleMode(cycleMode++);
+    };
 
     return activeMode;
 };
