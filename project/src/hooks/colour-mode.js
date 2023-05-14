2939eedbf3967f118095192111dc473d1786b4fd
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-05-14 19:32:47 +0100

    mode map objects added key pair values and methods

--- project/src/hooks/colour-mode.js
+++ project/src/hooks/colour-mode.js
@@ -2,9 +2,48 @@ import { useState } from "react";
 
 // Contains mode objects
 const modes = new Map(
-    [0, {}],
-    [1, {}],
-    [2, {}],
+    [0, {
+        displayText: "Erase",
+        colour: "none",
+    }],
+
+    [1, {
+        displayText: "User Selected Colour",
+        colourPicker: document.getElementById("colour-picker"),
+
+        //Returns current value of colour picker element - Selected by user
+        colour: function() {
+            return this.colourPicker.value
+        },
+    }],
+
+    [2, {
+        displayText: "Random Colour",
+
+        // Retruns formatted RGB value string - rgb(000, 000, 000)
+        colour: function() {
+            // Retruns formatted string of values - 000, 000, 000
+            const genRgbValues = (createNumbers = 3) => {
+                let outString = "";
+
+                // Generate number between 0 - 255
+                const genNumber = (maxValue = 255) => {
+                    return String(Math.floor(Math.random() * maxValue));
+                };
+
+                // Construct string with required amount of numbers - Add ', ' to all but last number
+                for(let i = 0; i < createNumbers; i++) {
+                    const newNumber = genNumber();
+                    newNumber = !i === genNumber ? newNumber.concat(', ') : newNumber;
+                    outString.concat(newNumber);                 
+                };
+
+                return outString
+            };
+
+            return `rgb(${genRgbValues()})`
+        },
+    }],
 );
 
 //Cycle + retrun active mode object
