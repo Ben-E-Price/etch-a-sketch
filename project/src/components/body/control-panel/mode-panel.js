2050f7bbb055e240a132c4bbb75342dfcf1ea015
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-05-16 19:55:04 +0100

    ModePanel Basic componenet content Current mode disaply functionality

--- project/src/components/body/control-panel/mode-panel.js
+++ project/src/components/body/control-panel/mode-panel.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import useColourMode from "../../../hooks/colour-mode";
 
 const ModePanel = () => {
+    const {activeMode} = useColourMode();
+    const [modeText, setModeText] = useState("")
 
+    return(
+        <div id="mode-panel">
+            <div id="mode-display">
+                <h2>Current Mode</h2>
+                <h3 id="current-mode-display">{activeMode.displayText}</h3>
+            </div>
+        </div>
+    )
 };
 
 export default ModePanel;
