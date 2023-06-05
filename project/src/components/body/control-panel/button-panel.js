4382fa849d2a6783238f947b37ce22fcc3f8479d
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-06-05 20:12:03 +0100

    Created buttonData clickEventFuncs btnObjectConst btnInfoObjects

--- project/src/components/body/control-panel/button-panel.js
+++ project/src/components/body/control-panel/button-panel.js
@@ -1,5 +1,40 @@
 import Button from './button';
 import useSwitchMode from '../../../hooks/cycle-mode';
+import { useState } from 'react';
+
+//Functions called on click events
+const buttonFunctions = {
+    colourMode: function({switchFunc}) {
+
+    },
+
+    erase: function () {
+
+    },
+
+    clearBoard: function() {
+        
+    },
+
+    resetBoard: function() {
+        
+    },
+
+    toggleGrid: function() {
+        
+    },
+};
+
+const getFuncs = funcObject => funcName => funcObject[funcName]; 
+const getBtnFuncs = getFuncs(buttonFunctions);
+
+// Object constructor - Returns data structure required by button creation
+const btnInfoConst =  (text, funcName) => {
+    return {
+        text,
+        func: getBtnFuncs(funcName) 
+    };
+};
 
 // Contains objects representing buttons
 const btnInfo = {
@@ -16,6 +51,28 @@ const createButton = ([btnName, infoObj]) => {
 };
 
 const ButtonPanel = () => {
+    const [toggleMode, setToggleMode] = useState("");
+    const activeMode = useSwitchMode(toggleMode);
+
+    //Contains infomation required during button creration 
+    const buttonData = {
+
+        // Functions called on click evnets
+        clickEventFuncs: {
+
+        },
+
+        // Construct object containg button infomation - Button Text - Click event function
+        btnObjectConst: function() {
+
+        },
+
+        // Contains button infomation objects 
+        btnInfoObjects: {
+
+        },
+    };
+
     const buttons = Object.entries(btnInfo).map((btnInfoObj) => createButton(btnInfoObj))
 
     return(
