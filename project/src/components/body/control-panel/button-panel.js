2ce4bdd1be5acf698e89dae38adca0dd969f377c
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-06-05 20:14:00 +0100

    Removed unrequired button creation functionality

--- project/src/components/body/control-panel/button-panel.js
+++ project/src/components/body/control-panel/button-panel.js
@@ -2,54 +2,6 @@ import Button from './button';
 import useSwitchMode from '../../../hooks/cycle-mode';
 import { useState } from 'react';
 
-//Functions called on click events
-const buttonFunctions = {
-    colourMode: function({switchFunc}) {
-
-    },
-
-    erase: function () {
-
-    },
-
-    clearBoard: function() {
-        
-    },
-
-    resetBoard: function() {
-        
-    },
-
-    toggleGrid: function() {
-        
-    },
-};
-
-const getFuncs = funcObject => funcName => funcObject[funcName]; 
-const getBtnFuncs = getFuncs(buttonFunctions);
-
-// Object constructor - Returns data structure required by button creation
-const btnInfoConst =  (text, funcName) => {
-    return {
-        text,
-        func: getBtnFuncs(funcName) 
-    };
-};
-
-// Contains objects representing buttons
-const btnInfo = {
-    colourToggle: btnInfoConst('Colour Mode', 'colourMode'),
-    erase: btnInfoConst('Eraser', 'erase'),
-    clear: btnInfoConst('Clear Board', 'clearBoard'),
-    resetBoard: btnInfoConst('Reset Board', 'resetBoard'),
-    gridToggle: btnInfoConst('Toggle Grid Lines', 'toggleGrid'),
-};
-
-// Return Button component + Add button content
-const createButton = ([btnName, infoObj]) => {
-    return <Button id={btnName} textCont={infoObj.text} clickEventFunction={infoObj.func}/>
-};
-
 const ButtonPanel = () => {
     const [toggleMode, setToggleMode] = useState("");
     const activeMode = useSwitchMode(toggleMode);
