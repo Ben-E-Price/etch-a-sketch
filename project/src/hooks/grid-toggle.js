45d1d259c4295fd5ec80945341c7842482e030c7
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-04 18:56:53 +0100

    useGridToggle declared required states

--- project/src/hooks/grid-toggle.js
+++ project/src/hooks/grid-toggle.js
@@ -1,7 +1,8 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 
 const useGridToggle = () => {
-
+    const [stateToggle, setStateToggle] = useState(false);
+    const [outString, setOutString] = useState("none");
 };
 
 export default useGridToggle
