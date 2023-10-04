58f752d75ece39ea6b568ac861dbbe250e712c02
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-04 19:01:26 +0100

    handleClick func declaration Will be passed/invoked via toggle grid button

--- project/src/hooks/grid-toggle.js
+++ project/src/hooks/grid-toggle.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 const useGridToggle = () => {
     const [stateToggle, setStateToggle] = useState(false);
     const [outString, setOutString] = useState("none");
+
+    const handleClick = () => {
+
+    };
 };
 
 export default useGridToggle
