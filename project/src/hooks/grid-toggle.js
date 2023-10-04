c554220f2898cd02e11651b10c977c917f6649c8
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-04 19:24:24 +0100

    handleClick func Declared ternary op Calls handleState Parameters passed dependent of stateCheck value

--- project/src/hooks/grid-toggle.js
+++ project/src/hooks/grid-toggle.js
@@ -4,11 +4,14 @@ const useGridToggle = () => {
     const [stateToggle, setStateToggle] = useState(false);
     const [outString, setOutString] = useState("none");
 
-    const handleClick = () => {
+    const handleClick = (stateCheck = stateToggle) => {
         const handleStateChange = (toggleSwitch, stringSwitch) => {
             setOutString(stringSwitch);
             setStateToggle(toggleSwitch);
         };
+
+        // stateCheck === true, hide grid - stateCheck === false, display grid 
+        stateCheck ? handleStateChange(false, "none") : handleStateChange(true, "")
     };
 };
 
