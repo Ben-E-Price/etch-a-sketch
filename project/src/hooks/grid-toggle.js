08b754e7de089c6b70cad061248b150a33c06bd3
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-04 19:28:19 +0100

    Variables declared hideStyle + displayStyle

--- project/src/hooks/grid-toggle.js
+++ project/src/hooks/grid-toggle.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const useGridToggle = () => {
     const [stateToggle, setStateToggle] = useState(false);
-    const [outString, setOutString] = useState("none");
+    const [outString, setOutString] = useState('none');
 
     const handleClick = (stateCheck = stateToggle) => {
+        const hideStyle = 'none';
+        const displayStyle = '1px solid black';
+
         const handleStateChange = (toggleSwitch, stringSwitch) => {
             setOutString(stringSwitch);
             setStateToggle(toggleSwitch);
         };
 
         // stateCheck === true, hide grid - stateCheck === false, display grid 
-        stateCheck ? handleStateChange(false, "none") : handleStateChange(true, "")
+        stateCheck ? handleStateChange(false, hideStyle) : handleStateChange(true, displayStyle);
     };
 };
 
