0242f497e897be67a6da5adfd4a75780eee6a5fe
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-10 19:05:15 +0100

    clear-board.js custom hook created Includes boiler plate

---
+++ project/src/hooks/clear-board.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const useGridClear = () => {
+
+    const handleClick = (stateCheck = stateToggle) => {
+
+    };
+
+    return {}
+};
+
+export default useGridClear
