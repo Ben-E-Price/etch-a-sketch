ab6a97a436e6d04848860fcc819f7176d0a2be0c
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-10 19:16:07 +0100

    useClearBoard hook functionality Loop els containing className Set el background Remove className

--- project/src/hooks/clear-board.js
+++ project/src/hooks/clear-board.js
@@ -1,12 +1,20 @@
 import { useState } from 'react';
 
-const useGridClear = () => {
+const useClearBoard = () => {
 
-    const handleClick = (stateCheck = stateToggle) => {
+    const handleClick = () => {
+        const className = "active-background";
+        const activePixels = document.getElementsByClassName(className);
 
+        const clearPixel = (pixel, className) => {
+            pixel.style.background = "none";
+            pixel.classList.remove(className);
+        };
+
+        activePixels.forEach(element => clearPixel(element, className));
     };
 
-    return {}
+    return {handleClick}
 };
 
-export default useGridClear
+export default useClearBoard
