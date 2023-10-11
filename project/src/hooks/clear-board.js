2d553fff76475e8d60ef6345501e2938f597c07f
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-11 14:51:08 +0100

    BugFix useClearBoard unable to execute activePixels.forEach loop Prevents pixel clearing Bug 	-activePixels.forEach execution failure 		-Resulting error "Unable to loop HTMLcollection"
    Bug Cause
    	-activePixels value declaration "document.getElementByClassName(className)"
    		-Returns HTML collection object not Array
    
    Bug Fix
    	-activePixels value re-declaration using spread operator
    		-[...document.getElementByClassName(className)]
    		-Inserts HTML elements into array positions
    		-Enables activePixel looping

--- project/src/hooks/clear-board.js
+++ project/src/hooks/clear-board.js
@@ -4,14 +4,14 @@ const useClearBoard = () => {
 
     const handleClick = () => {
         const className = "active-background";
-        const activePixels = document.getElementsByClassName(className);
+        const activePixels = [...document.getElementsByClassName(className)];
 
         const clearPixel = (pixel, className) => {
             pixel.style.background = "none";
             pixel.classList.remove(className);
         };
-
-        activePixels.forEach(element => clearPixel(element, className));
+        
+        activePixels.forEach((element) => clearPixel(element, className));
     };
 
     return handleClick
