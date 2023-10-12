ec3b41328354ff61798153a91a55d6e7d3b8f6c3
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-12 19:32:51 +0100

    Body comp removed handleResolutionChange currentSettings declerations Invoked useUpdateGridResolution

--- project/src/components/body/body.js
+++ project/src/components/body/body.js
@@ -3,53 +3,13 @@ import PixelBoard from "./board/pixel-board";
 import React, { useState } from "react";
 import useColourMode from "../../hooks/colour-mode";
 import useGridToggle from "../../hooks/grid-toggle";
+import useUpdateGridResolution from "../../hooks/grid-resolution";
 
 function Body() {
     const [newMode, setNewMode] = useState("");
     const {activeMode} = useColourMode(newMode);
     const gridToggle = useGridToggle();
-
-    const [currentSettings, updateSettings] = useState({
-        gridResolution: 16,
-        inputStep: 16,
-        inputMin: 0,
-    });
-
-    const handleResoultionChange = (ref) => {
-        const prevRes = currentSettings.gridResolution;
-        const currentValue = Number(ref.value);
-
-        const updateState = stateKey => (newStateValue) => updateSettings(previousState => {         
-            return {...previousState, [stateKey]: newStateValue};
-        })
-
-        const updateCurrentRes = updateState('gridResolution');
-        const updateInputMin = updateState('inputMin');
-        const updateInputStep = updateState('inputStep');
-
-        // Blocks below minimum input values - Sets input elements min value 
-        const inputLimit = (inputValue) => {
-            const minValue = 4;
-            inputValue <= minValue ? updateInputMin(minValue) : updateInputMin(0);
-        };
-        
-        // Update inputStep value - Increase resoulution value
-        const resIncrease = () => {
-            updateInputStep(prevRes);
-            updateCurrentRes(prevRes * 2);
-        };
-        
-        // Update inputStep value - Decrease resoulution value 
-        const resDecrease = () => {
-            const decreasedRes = prevRes / 2;
-
-            updateInputStep(decreasedRes);
-            updateCurrentRes(decreasedRes);
-        };
-
-        inputLimit(currentValue);
-        prevRes < currentValue ? resIncrease() : resDecrease();
-    };
+    const {handleResoultionChange, handleReset, currentSettings} = useUpdateGridResolution();
 
     return(
         <div id="main-content" className="flex-row">
