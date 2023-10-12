e22cb9a7c65141aed4deb67bfdf83b9109e06170
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-12 18:59:12 +0100

    grid-resolution.js created

---
+++ project/src/hooks/grid-resolution.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const useUpdateGridResolution = () => {
+
+    const handleResoultionChange = () => {
+
+    };
+
+    return {}
+};
+
+export default useUpdateGridResolution
