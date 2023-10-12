6f13ff5d0d63754f6dd6e775b10db4e80285957e
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-12 19:15:58 +0100

    handleResoultionChange functionality added from Body comp function

--- project/src/hooks/grid-resolution.js
+++ project/src/hooks/grid-resolution.js
@@ -11,7 +11,39 @@ const useUpdateGridResolution = () => {
 
     // Sets gird resoultion based on "ref" (gridResoultion compoent) value
     const handleResoultionChange = (ref) => {
+        const prevRes = currentSettings.gridResolution;
+        const currentValue = Number(ref.value);
 
+        const updateState = stateKey => (newStateValue) => updateSettings(previousState => {         
+            return {...previousState, [stateKey]: newStateValue};
+        })
+
+        const updateCurrentRes = updateState('gridResolution');
+        const updateInputMin = updateState('inputMin');
+        const updateInputStep = updateState('inputStep');
+
+        // Blocks below minimum input values - Sets input elements min value 
+        const inputLimit = (inputValue) => {
+            const minValue = 4;
+            inputValue <= minValue ? updateInputMin(minValue) : updateInputMin(0);
+        };
+        
+        // Update inputStep value - Increase resoulution value
+        const resIncrease = () => {
+            updateInputStep(prevRes);
+            updateCurrentRes(prevRes * 2);
+        };
+        
+        // Update inputStep value - Decrease resoulution value 
+        const resDecrease = () => {
+            const decreasedRes = prevRes / 2;
+
+            updateInputStep(decreasedRes);
+            updateCurrentRes(decreasedRes);
+        };
+
+        inputLimit(currentValue);
+        prevRes < currentValue ? resIncrease() : resDecrease();
     };
 
     // Reset currentSettings to defaultSettings value - Resets grid UI 
@@ -19,7 +51,7 @@ const useUpdateGridResolution = () => {
         updateSettings(defaultSettings);
     };
 
-    return {handleResoultionChange, handleReset}
+    return {handleResoultionChange, handleReset, currentSettings}
 };
 
 export default useUpdateGridResolution
