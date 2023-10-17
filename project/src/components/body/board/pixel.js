98b55b3cd256db1965557f853487e56c04e61054
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-10-17 16:38:54 +0100

    updatePixelColour scope change Pixel > eventCheck

--- project/src/components/body/board/pixel.js
+++ project/src/components/body/board/pixel.js
@@ -4,54 +4,54 @@ import useColourMode from "../../../hooks/colour-mode";
 function Pixel(prop) {
     const activeMode = prop.activeMode;
 
-    // Set background colour of component
-    const updatePixelColour = (ref, activeMode) => {
-        const {modeIdent, colour} = activeMode;
-
-        // Adds/Removes "active-background" class from compoents - Allows tracking of compoents with backgrounds applied
-        const setActiveClass = (modeIdent, pixel) => {
-            const className = "active-background";
-            const modeCheck = modeIdent !== 0 ? true : false;
-
-            const classAdd = (pixel, className) => pixel.classList.add(className);
-            const classRemove = (pixel, className) => pixel.classList.remove(className);
-
-            const checkClass = (pixel, className) => pixel.classList.contains(className);
-
-            if(modeCheck && !checkClass(pixel, className)) {
-                // Add class if user selected or random colour mode
-                classAdd(pixel, className);
-            } else if (!modeCheck && checkClass(pixel, className)) {
-                // Remove class if erase mode
-                classRemove(pixel, className);
-            };
-        };
-
-        // Sets static user selected colour value
-        const setStaticColour = (pixel, newColour) => {
-            pixel.style.background = newColour
-        };
-
-        // Calls random colour function, Sets pixel colour
-        const setRandomColour = (pixel, colourFunc) => {
-            pixel.style.background = colourFunc();
-        };
-
-        setActiveClass(modeIdent, ref)
-        
-        // Selects colour setting method based on current mode
-        if(modeIdent === 1 || modeIdent === 0) {
-            setStaticColour(ref, colour);
-        } else if(modeIdent === 2) {
-            setRandomColour(ref, colour);
-        };
-    };
-
     // Check if colour should be added to component based on invoction event
     const eventCheck = (event, mouseDownState, activeMode) => {
         const {type, target} = event;
         const eventClick = "click";
         const eventOver = "mouseover";
+
+        // Set background colour of component
+        const updatePixelColour = (ref, activeMode) => {
+            const {modeIdent, colour} = activeMode;
+
+            // Adds/Removes "active-background" class from compoents - Allows tracking of compoents with backgrounds applied
+            const setActiveClass = (modeIdent, pixel) => {
+                const className = "active-background";
+                const modeCheck = modeIdent !== 0 ? true : false;
+
+                const classAdd = (pixel, className) => pixel.classList.add(className);
+                const classRemove = (pixel, className) => pixel.classList.remove(className);
+
+                const checkClass = (pixel, className) => pixel.classList.contains(className);
+
+                if(modeCheck && !checkClass(pixel, className)) {
+                    // Add class if user selected or random colour mode
+                    classAdd(pixel, className);
+                } else if (!modeCheck && checkClass(pixel, className)) {
+                    // Remove class if erase mode
+                    classRemove(pixel, className);
+                };
+            };
+
+            // Sets static user selected colour value
+            const setStaticColour = (pixel, newColour) => {
+                pixel.style.background = newColour
+            };
+
+            // Calls random colour function, Sets pixel colour
+            const setRandomColour = (pixel, colourFunc) => {
+                pixel.style.background = colourFunc();
+            };
+
+            setActiveClass(modeIdent, ref)
+            
+            // Selects colour setting method based on current mode
+            if(modeIdent === 1 || modeIdent === 0) {
+                setStaticColour(ref, colour);
+            } else if(modeIdent === 2) {
+                setRandomColour(ref, colour);
+            };
+        };
 
         // Check invoction event type
         if(type === eventOver) {
