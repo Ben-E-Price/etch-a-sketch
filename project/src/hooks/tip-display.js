e4d0602671f0492bbaf964940d680abea5b9fbfe
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-03 14:34:34 +0000

    updateCompData func created Loop passed object update compData state values

--- project/src/hooks/tip-display.js
+++ project/src/hooks/tip-display.js
@@ -10,6 +10,15 @@ const useToolTipDisplay = () => {
 
     const [createTimer, setCreateTimer] = useState("")
     const [timer, setTimer] = useState("");
+
+    // Update compData state key pair values
+    const updateCompData = (keyPairs) => {
+        for(const [key, value] in keyPairs.entries()) {
+            setCompData(prevState => {
+                return {...prevState, [key]: value}
+            });
+        };
+    };
 };
 
 export default useToolTipDisplay
