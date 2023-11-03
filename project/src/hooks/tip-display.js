5a98e18f444285d94733e62d27a1aedf75310768
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-03 14:04:12 +0000

    useToolTipDisplay state declarations compData createTimer timer

--- project/src/hooks/tip-display.js
+++ project/src/hooks/tip-display.js
@@ -1,7 +1,15 @@
 import { useState } from "react";
 
 const useToolTipDisplay = () => {
+    const [compData, setCompData] = useState({
+        compText: "",
+        compDisplay: false,
+        pageLocX: "",
+        pageLocY: "",
+    });
 
+    const [createTimer, setCreateTimer] = useState("")
+    const [timer, setTimer] = useState("");
 };
 
 export default useToolTipDisplay
