d81abf5da62adeafcff12172bcc883863e928d0f
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-05 14:55:53 +0000

    Body component useToolTipDisplay invoction

--- project/src/components/body/body.js
+++ project/src/components/body/body.js
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 import useColourMode from "../../hooks/colour-mode";
 import useGridToggle from "../../hooks/grid-toggle";
 import useUpdateGridResolution from "../../hooks/grid-resolution";
+import useToolTipDisplay from "../../hooks/tip-display";
 
 function Body() {
     const [newMode, setNewMode] = useState("");
     const {activeMode} = useColourMode(newMode);
     const gridToggle = useGridToggle();
     const {handleResolutionChange, handleGridReset, currentSettings} = useUpdateGridResolution();
+    const {handleMouseEvent, compData} = useToolTipDisplay();
 
     return(
         <div id="main-content" className="flex-row">
