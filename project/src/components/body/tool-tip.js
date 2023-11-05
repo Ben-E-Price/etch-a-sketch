77bc2988e7b5fe0a768a4c8a32be88d8e9964e43
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-05 14:39:52 +0000

    ToolTip comp compData destructuring Declared disClass variable

--- project/src/components/body/tool-tip.js
+++ project/src/components/body/tool-tip.js
@@ -1,9 +1,11 @@
 
 const ToolTip = (prop) => {
+    const {compText, compDisplay, pageLocX, pageLocY} = prop.compData;
+    const disClass = compDisplay ? "dis-flex" : "dis-none";
 
     return(
-        <div id="tool-wrapper">
-
+        <div id="tool-wrapper" className={disClass}>
+            <p>{compText}</p>
         </div>
     )
 };
