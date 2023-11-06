6ae82fc6fd28316418cc4be0ba67f60a97793f29
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-06 15:17:28 +0000

    Button comp toolTipFn invoction onMouseEnter onMouseLeave events

--- project/src/components/body/control-panel/button.js
+++ project/src/components/body/control-panel/button.js
@@ -4,7 +4,7 @@ const Button = (prop) => {
     const ref = useRef("");
 
     return(
-        <button id={prop.id} ref={ref} onClick={prop.clickEventFunction}>
+        <button id={prop.id} ref={ref} onClick={prop.clickEventFunction} onMouseEnter={(event) => prop.toolTipFn(event)} onMouseLeave={(event) => prop.toolTipFn(event)}>
             {prop.textCont}
         </button>
     )
