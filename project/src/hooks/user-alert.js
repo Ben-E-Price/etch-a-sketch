2453a4667223cb4695010b88a21f9bf6dbae33b8
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-20 16:13:40 +0000

    updateBlockedFn func decleration Loop blockedFn obj Set values to newState param values

--- project/src/hooks/user-alert.js
+++ project/src/hooks/user-alert.js
@@ -6,7 +6,18 @@ const useUserAlert = () => {
     const [blockedFn, setBlockedFn] = useState({
         fn: false,
         args: [],
-    })
+    });
+
+    // Update all blockedFn state keypairs with newState values
+    const updateBlockedFn = (newState, currentState = blockedFn) => {
+
+        for(const [index, key] of Object.keys(currentState)) {
+
+            setBlockedFn((prevState) => {
+                return {...prevState, [key]: newState[index]}
+            });
+        };
+    };
 };
 
 export default useUserAlert
