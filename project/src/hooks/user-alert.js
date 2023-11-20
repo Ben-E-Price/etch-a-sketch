2914992967c0f8cb67c2e0f03a5fad34430cf54c
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-20 16:32:20 +0000

    alertInput func decleration Hide Alert comp Execute callback based on user input

--- project/src/hooks/user-alert.js
+++ project/src/hooks/user-alert.js
@@ -19,11 +19,21 @@ const useUserAlert = () => {
         };
     };
 
-    // Initialize user alert - Set alert text content - Set callback ref  - Display alert comp
+    // Called on user input - Initialize user alert - Set alert text content - Set callback ref  - Display alert comp
     const alertInit = (callbackFn, fnArgs, alertText) => {
         setAlertText(alertText);
         updateBlockedFn([callbackFn, fnArgs]);
         setDisplayAlert(true);
+    };
+
+    // Called on user input via alert comp - Hide alert comp - Execute blocked inital function on confirm
+    const alertInput = (userConfirm, blockedFn) => {
+        setDisplayAlert(false);
+
+        if(userConfirm) {
+            const [callback, fnArgs] = blockedFn;
+            callback(...fnArgs);
+        };
     };
 };
 
