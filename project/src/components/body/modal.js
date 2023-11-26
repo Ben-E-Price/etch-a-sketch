7f278d0e94c8e7b6b0fb57a4810c18956b217145
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-11-26 18:50:31 +0000

    Modal component Created JSX button elements

--- project/src/components/body/modal.js
+++ project/src/components/body/modal.js
@@ -6,10 +6,18 @@ const Modal = (prop) => {
         <div id="modal">
                 
             <div id="modal-wrapper" className={colClass}>
-                <h1 id="modal-heading">Caution</h1>
-                <p id="modal-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet. 
-                </p>
+
+                <div id="modal-text-wrapper">
+                    <h1 id="modal-heading">Caution</h1>
+                    <p id="modal-text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet. 
+                    </p>
+                </div>
+
+                <div id="modal-button-wrapper">
+                    <button className="modal-button">Continue</button>
+                    <button className="modal-button">Cancel</button>
+                </div>
             </div>
         </div>
     )
