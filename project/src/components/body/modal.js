518ea69f6270d09c0e9000e5ec1a7730394a952b
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2023-12-05 18:37:09 +0000

    Modal comp modal-text el passed modalText value

--- project/src/components/body/modal.js
+++ project/src/components/body/modal.js
@@ -10,7 +10,7 @@ const Modal = (prop) => {
                 <div id="modal-text-wrapper">
                     <h1 id="modal-heading">Caution</h1>
                     <p id="modal-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce imperdiet. 
+                        {modalText}
                     </p>
                 </div>
 
