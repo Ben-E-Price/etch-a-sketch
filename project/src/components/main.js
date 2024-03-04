5fa2aa0070447057849e2fc24ce19553b97f7a0e
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2024-03-04 16:23:14 +0000

    Main useModal destructuring multi-line Body comp props declaration multi-line

--- project/src/components/main.js
+++ project/src/components/main.js
@@ -3,13 +3,22 @@ import Body from "./body/body";
 import useModal from "../hooks/modal-display";
 
 const Main = () => {
-    const {modalInit, modalInput, displayModal, blockedElStyles, modalData, modalCancelled} = useModal()
-    const {modalHeight} = modalData;
+    const { modalInit,
+            modalInput,
+            displayModal,
+            blockedElStyles,
+            modalData,
+            modalCancelled } = useModal()
+    const { modalHeight } = modalData;
 
     return(
         <main style={displayModal ? {height: modalHeight} : {}}>
             {displayModal ? <Modal modalInput={modalInput} modalData={modalData}/> : <></>}
-            <Body modal={{modalInit, modalCancelled}} blockedElStyles={blockedElStyles} displayModal={displayModal}/>
+            
+            <Body
+                modal={{modalInit, modalCancelled}}
+                blockedElStyles={blockedElStyles}
+                displayModal={displayModal}/>
         </main>
     )
 }
