bcf155efaaf033db7ea7cd3513bdf73b666e7295
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2024-03-26 16:22:48 +0000

    handleClassStrings functionality added

--- project/src/hooks/const-class-string.js
+++ project/src/hooks/const-class-string.js
@@ -4,13 +4,28 @@ const useClassStringConstruct = () => {
     //Create string containing mutiple HTML class names
     const handleClassStrings = (inStrings) => {
 
-        // Remove values not of type string
+        // Remove none string values
         const cleanArray = (checkArray) => {
             const checkType = (check) => check === typeof "string" ? true : false;  
 
             const cleanArray = checkArray.filter((value) => checkType(value));
             return cleanArray
         };
+
+        // Return, type is not array - empty array  
+        if(!Array.isArray(inStrings) || inStrings.length === 0) {
+            return
+        };
+
+        const classStrings = cleanArray(inStrings);
+
+        if(classStrings.length === 1) {
+            //Single string present
+            return classStrings[0]
+        } else {
+            // Multiple strings present
+            return classStrings.join(" ")
+        }
     };
 
     return {handleClassStrings}
