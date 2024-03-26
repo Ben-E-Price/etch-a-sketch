c46458e115ff46022422045a1a4ff46ca00eb394
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2024-03-26 16:29:16 +0000

    cleanString func declared Created functionality ensures strings adhear to HTML class naming standards

--- project/src/hooks/const-class-string.js
+++ project/src/hooks/const-class-string.js
@@ -12,12 +12,18 @@ const useClassStringConstruct = () => {
             return cleanArray
         };
 
+        // Ensure HTML class naming standards - "class-naming"
+        const cleanString = (inString) => {
+            return inString.trim().replace(" ", "-").toLower()
+        }
+
         // Return, type is not array - empty array  
         if(!Array.isArray(inStrings) || inStrings.length === 0) {
             return
         };
 
         const classStrings = cleanArray(inStrings);
+        classStrings.forEach(element => cleanString(element));
 
         if(classStrings.length === 1) {
             //Single string present
@@ -25,7 +31,7 @@ const useClassStringConstruct = () => {
         } else {
             // Multiple strings present
             return classStrings.join(" ")
-        }
+        };
     };
 
     return {handleClassStrings}
