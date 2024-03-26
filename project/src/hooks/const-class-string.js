e1f66a6e8e6391a907026ee53ff991f6fb303d91
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2024-03-26 15:01:24 +0000

    Declared funcs cleanArray checkType Remove all none string values Return array of strings

--- project/src/hooks/const-class-string.js
+++ project/src/hooks/const-class-string.js
@@ -4,6 +4,13 @@ const useClassStringConstruct = () => {
     //Create string containing mutiple HTML class names
     const handleClassStrings = (inStrings) => {
 
+        // Remove values not of type string
+        const cleanArray = (checkArray) => {
+            const checkType = (check) => check === typeof "string" ? true : false;  
+
+            const cleanArray = checkArray.filter((value) => checkType(value));
+            return cleanArray
+        };
     };
 
     return {handleClassStrings}
