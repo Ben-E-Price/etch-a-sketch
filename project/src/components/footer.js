4a942b6573bcb5b8d3fcd47a4e8509909b58c315
Ben Price <96444313+Ben-E-Price@users.noreply.github.com>
2024-04-18 15:52:51 +0100

    Footer inital styling

--- project/src/components/footer.js
+++ project/src/components/footer.js
@@ -1,23 +1,60 @@
 import data from "data/comp-class-list.json"
+import useClassStringConstruct from "hooks/create-class-string";
 
 const Footer = () => {
+    const handleClassString = useClassStringConstruct();
     const {
+        flexCol,
         flexRow,
+        gitLink,
+        gitLogo
     } = data;
     const date = new Date().getFullYear();
 
     return(
     <footer className={flexRow}>
-        <div id="link-wrapper"
-            className={flexRow}
+        <div id="prof-wrapper"
+            className={flexCol}
         >
-            <a href="https://github.com/Ben-E-Price"
+            <div id="git-wrapper"
                 className={flexRow}
-                id="git-link"
             >
-                <p>Ben Price {date}</p>
-                <img src="images/GitHub-Mark-64px.png" id="logo-git"></img>
-            </a>
+                <span 
+                    id="logo-wrapper"
+                    className={flexRow}
+                >
+                    <img 
+                        src="images/GitHub-Logo.svg"
+                        id="git-logo-text"
+                        className={gitLogo}/>
+                    <img 
+                        src="images/GitHub-Mark-64px.png"
+                        id="git-logo-cat"
+                        className={gitLogo}/>
+                </span>
+                
+                <span 
+                    id="link-wrapper"
+                    className={flexCol}
+                >                    
+                    <a
+                        href="https://github.com/Ben-E-Price"
+                        className={gitLink}
+                    >
+                        <h4 id="git-link-prof"> Git Profile </h4>
+                    </a>
+
+                    <a
+                        href="https://github.com/Ben-E-Price"
+                        className={gitLink}
+                    >
+                        <h4 id="git-link-repo"> Project Repo </h4>
+                    </a>
+                </span>
+            </div>
+
+            <p>Ben Price {date}</p>
+
         </div>
     </footer>
     )
